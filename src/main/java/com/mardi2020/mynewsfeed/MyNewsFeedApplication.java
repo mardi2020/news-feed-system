528a717d5971e0774ee92a6ef04ed6470f6d4e0a
mardi2020 <mardi@khu.ac.kr>
2024-06-09 19:43:27 +0900

    chore: inital project code

---
+++ src/main/java/com/mardi2020/mynewsfeed/MyNewsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.mardi2020.mynewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewsFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNewsFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mardi2020/mynewsfeed/MyNewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardi2020.mynewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewsFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
